5a948b298e744693c910c782110a6f34b429c02f
Abhishek <jadonabhi0@gmail.com>
2022-11-24 14:44:18 +0530

    Hare Krishna

---
+++ src/Client_pep.java
@@ -0,0 +1,17 @@
+public class Client_pep {
+    public static void main(String[] args) {
+
+        Graph_Pep graph = new Graph_Pep(7,8);
+            graph.addEdge(0,1,10);
+            graph.addEdge(1,2,10);
+            graph.addEdge(2,3,10);
+            graph.addEdge(0,3,10);
+            graph.addEdge(3,4,10);
+            graph.addEdge(4,5,10);
+            graph.addEdge(5,6,10);
+            graph.addEdge(4,6,10);
+
+            graph.display();
+
+    }
+}
---
+++ src/Graph_Pep.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Graph_Pep {
+
+    // this class contains the details of edges of graphs.
+    static class Edge{
+        int src;
+        int nbr;
+        int wt;
+        public Edge(int src, int nbr, int wt){
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
+    ArrayList<Edge>[] graph;
+    public Graph_Pep(int vertex,int edges){
+       graph = new ArrayList[vertex];
+       for (int i = 0 ; i < vertex ; i++){
+           graph[i] = new ArrayList<>();
+       }
+   }
+
+
+    public void addEdge(int src, int nbr, int wt){
+        Edge edge1 = new Edge(src, nbr, wt);
+        Edge edge2 = new Edge(nbr, src, wt);
+        graph[src].add(edge1);
+        graph[nbr].add(edge2);
+   }
+
+
+
+
+    private void Display_(int size){
+        for (int i = 0 ; i < size ; i++){
+            System.out.println(i +" --> {"+graph[i].get(0).src+", "+graph[i].get(0).nbr+", "+graph[i].get(0).wt+"} ");
+
+        }
+   }
+
+   public void display(){
+        Display_(graph.length);
+   }
+
+
+
+
+}
