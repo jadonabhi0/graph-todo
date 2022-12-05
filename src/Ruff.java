3975272280c8eb5da296736f26eb5c479d448ac2
Abhishek <jadonabhi0@gmail.com>
2022-12-05 23:47:26 +0530

    isCyclic

--- src/Ruff.java
+++ src/Ruff.java
@@ -7,6 +7,27 @@ public class Ruff {
     }
 
 
+    int moveApples(int input1, int[] input2){
+        int avg = 0;
+        int sum = 0;
+
+        for(int i : input2){
+            sum += i;
+        }
+        avg = sum / input1;
+
+
+
+        int ans = 0;
+        for(int i : input2){
+            if (avg > i){
+                ans = ans + Math.abs(avg - i);
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] array = new int[]{1,2,3,4,5};
         String s = new String(array,0, array.length);
