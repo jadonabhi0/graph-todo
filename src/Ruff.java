92f737b45c79abd02c4e5f110f760d9b3f01042b
Abhishek <jadonabhi0@gmail.com>
2023-04-08 22:31:38 +0530

    ..

--- src/Ruff.java
+++ src/Ruff.java
@@ -4,7 +4,10 @@ public class Ruff {
 
     public static void main(String[] args) {
 
-        
+        char c1 = '1';
+        char c2 = '2';
+        int n = Integer.parseInt(c1+""+c2);
+        System.out.println(n);
 
     }
 
