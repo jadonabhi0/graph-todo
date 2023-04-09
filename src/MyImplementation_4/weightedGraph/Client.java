53f1359a0b89a2e45a103fbb159cfae9cd69a967
Abhishek <jadonabhi0@gmail.com>
2023-04-09 21:40:14 +0530

    implement Dijkstra

---
+++ src/MyImplementation_4/weightedGraph/Client.java
@@ -0,0 +1,19 @@
+package MyImplementation_4.weightedGraph;/*
+    @author jadon
+*/
+
+public class Client {
+    public static void main(String[] args) {
+        Graph graph = new Graph(7);
+        graph.addEdge(0, 1, 2);
+        graph.addEdge(0, 2, 6);
+        graph.addEdge(1, 3, 5);
+        graph.addEdge(2, 3, 8);
+        graph.addEdge(3, 5, 15);
+        graph.addEdge(3, 4, 10);
+        graph.addEdge(4, 5, 6);
+        graph.addEdge(4, 6, 2);
+        graph.addEdge(5, 6, 6);
+        graph.Dijkstra(0);
+    }
+}
---
+++ src/MyImplementation_4/weightedGraph/Graph.java
@@ -0,0 +1,64 @@
+package MyImplementation_4.weightedGraph;/*
+    @author jadon
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+
+public class Graph {
+
+    // ths is 0-indexed weighted graph
+    ArrayList<int[]>[] map;
+
+    public Graph(int n){
+        map = new ArrayList[n];
+        for(int i = 0; i < n ; i++){
+            map[i] = new ArrayList<>();
+        }
+    }
+
+    public void addEdge(int src, int ver, int wt){
+        map[src].add(new int[]{ver, wt});
+        map[ver].add(new int[]{src, wt});
+    }
+
+    public void Dijkstra(int src){
+        // Time Complexity is E-log(V)
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        int[] dis = new int[map.length];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+
+        // new int[]{dis, node}
+        pq.add(new int[]{0, src});
+        dis[src] = 0;
+
+        while(!pq.isEmpty()){
+            int[] ar = pq.poll();
+            int rmNode = ar[1];
+            int rmDis = ar[0];
+
+            for(int[] nbr : map[rmNode]){
+                if(nbr[1]+rmDis < dis[nbr[0]]){
+                    dis[nbr[0]] = nbr[1]+rmDis;
+                    pq.add(new int[]{nbr[1]+rmDis, nbr[0]});
+                }
+            }
+        }
+
+        for(int i = 0 ; i < dis.length ; i++){
+            System.out.println(i+" -> "+dis[i]);
+        }
+
+    }
+
+
+}
